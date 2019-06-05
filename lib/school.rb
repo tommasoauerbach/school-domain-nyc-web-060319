5f17ba5d1dae226d45284f6967f0d05c14c966e3
tommaso auerbach <tsa302@nyu.edu>
2019-06-05 17:17:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,26 @@
 class School
 
-attr_accessor :roster, :student_name, :school_name
+attr_accessor :roster, :namen
 
-def initialize(school_name, grade)
-  @school_name = school_name
+def initialize(name)
+  @name = name
   @roster = {}
 end
 
 def add_student(student_name, grade)
-  if @roster[grade] << student_name
-  else
-    @roster[grade] = []
-    roster[grade] << student_name
-  end
+  roster[grade] ||= []
+  roster[grade] << student_name
+end
 
+def grade(student_grade)
+  roster[student_grade]
+end
+
+def sort
+  sorted = {}
+  roster.each do |grade, students|
+    sorted[grade] = students.sort
+  end
+  sorted
+end
 end
